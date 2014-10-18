0ae87cfb9fdb4fe158b0703bf8d9c993e706ea5b
Yuta Matsumura <tkmbmbr@gmail.com>
2014-10-18 23:58:40 +0900

    #08

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,23 +1,18 @@
-﻿var http = require('http');
+﻿var http = require('http'),
+    fs = require('fs');
 var settings = require('./settings');
 var server = http.createServer();
-var msg;
 server.on('request', function(req, res) {
-  switch (req.url) {
-    case '/about':
-      msg = "about this page";
-      break;
-    case '/profile':
-      msg = "about me";
-      break;
-    default:
-      msg = "wrong page";
-      break;
-  }
-  
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('hello from ' + msg);
-  res.end();
+  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('not found');
+      return res.end();
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
