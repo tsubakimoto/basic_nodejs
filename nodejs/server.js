fbc4e83ccfd1fe8ffc80f3112eff3db90af0f472
Yuta Matsumura <tkmbmbr@gmail.com>
2014-10-18 20:28:48 +0900

    #07

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,9 +1,22 @@
 ﻿var http = require('http');
 var settings = require('./settings');
 var server = http.createServer();
+var msg;
 server.on('request', function(req, res) {
+  switch (req.url) {
+    case '/about':
+      msg = "about this page";
+      break;
+    case '/profile':
+      msg = "about me";
+      break;
+    default:
+      msg = "wrong page";
+      break;
+  }
+  
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('hello world !!!');
+  res.write('hello from ' + msg);
   res.end();
 });
 server.listen(settings.port, settings.host);
