8dd689fa10158eed614fab7e198360f801323804
Yuta Matsumura <tkmbmbr@gmail.com>
2014-10-19 00:16:33 +0900

    #10

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,18 +1,20 @@
 ﻿var http = require('http'),
-    fs = require('fs');
+    fs = require('fs'),
+    ejs = require('ejs');
 var settings = require('./settings');
 var server = http.createServer();
+var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
+var n = 0;
 server.on('request', function(req, res) {
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write('not found');
-      return res.end();
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
+  n++;
+  var data = ejs.render(template, {
+    title: "hello",
+    content: "<strong>World!</strong>",
+    n: n
   });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(data);
+  res.end();
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
